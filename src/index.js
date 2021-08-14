39f6c11fe06f2028eda5dbbbce5ce5297c31f793
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-08-13 21:34:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    newTodo(event.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function newTodo(todoText) {
+  const li = document.createElement("li")
+  const checkbox = document.createElement("span")
+  li.textContent = todoText
+  checkbox.textContent = " 🔲"
+  checkbox.className = "checkbox"
+  checkbox.addEventListener("click", deleteTodo)
+  li.appendChild(checkbox)
+  document.querySelector("#tasks").appendChild(li)
+}
+
+function deleteTodo(e) {
+  console.log(e.target.parentNode.remove())
+}
